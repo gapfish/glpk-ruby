8328e836de19bc36c510d93a072a2d1fc7a21f56
Fladson Gomes <fladsonthiago@gmail.com>
2020-04-21 16:11:05 +0200

    Attach create and delete prob functions

--- lib/glpk.rb
+++ lib/glpk.rb
@@ -1,4 +1,10 @@
 require "glpk/version"
+require 'ffi'
 
 module GLPK
+  extend FFI::Library
+  ffi_lib ['libglpk']
+
+  attach_function :glp_create_prob, [], :pointer
+  attach_function :glp_delete_prob, [:pointer], :void
 end
--- spec/glpk/glpk_spec.rb
+++ spec/glpk/glpk_spec.rb
@@ -1,5 +1,13 @@
 RSpec.describe GLPK do
   it "has a version number" do
-    expect(GLPK::VERSION).not_to be nil
+    expect(GLPK::VERSION).not_to be_nil
+  end
+
+  describe 'glp_create_prob' do
+    let(:prob) { GLPK.glp_create_prob }
+
+    it "creates a glpk prob" do
+      expect(prob.class).to be(FFI::Pointer)
+    end
   end
 end
