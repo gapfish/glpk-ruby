f257ef84d497ff04de9db7a661a6dc8e445694e3
Fladson Gomes <fladsonthiago@gmail.com>
2020-04-24 19:29:47 +0200

    Update problem.rb

--- lib/glpk/problem.rb
+++ lib/glpk/problem.rb
@@ -1,15 +1,17 @@
 module GLPK
   class Problem
+    attr_accessor :problem
+
     def initialize
-      @lp = GLPK.glp_create_prob
+      @problem = GLPK.glp_create_prob
     end
 
     def name=(name)
-      GLPK.glp_set_prob_name(@lp, name)
+      GLPK.glp_set_prob_name(@problem, name)
     end
 
     def name
-      GLPK.glp_get_prob_name(@lp)
+      GLPK.glp_get_prob_name(@problem)
     end
   end
 end
